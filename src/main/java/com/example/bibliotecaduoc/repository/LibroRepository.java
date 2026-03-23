5cb49a6f86ca9715c9cef8f52e9d7a5b5c7ddb83
Raul <rau.barraza@duocuc.cl>
2026-03-23 19:50:23 -0300

    avance 2 bibloteca

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -47,7 +47,9 @@ public class LibroRepository {
 
     }
 
-    public Libro actualizar(Libro lib) {
+    // actualizar libro
+
+    public Libro actualizar (Libro lib) {
         int id = 0;
         int idPosicion = 0;
 
@@ -61,7 +63,49 @@ public class LibroRepository {
         }
 
         Libro libro1 = new Libro();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        listaLibros.set(idPosicion,libro1);
+        return libro1;
         
     }
+
+    // eliminar libro//
+
+    public void eliminar(int id) {
+        // alternativa 1 
+
+        Libro libro = buscarporid(id);
+        if (libro != null)  {
+            listaLibros.remove(libro); 
+        }
+
+        //Alternativa 2
+        int idPosicion = 0;
+        for (int i = 0; i < listaLibros.size(); i++ ){
+
+            if (libro != null) {
+                id idPosicion = i;
+                break;
+            }
+        } 
+        
+        if (idPosicion > 0) {
+            listaLibros.remove(idPosicion); 
+            
+        }
+
+        // Otra Alternativa 
+
+        listaLibros
+            
+    } 
+
+    // 
 
 }
